245a8bb025fc64747b610f788715c2ae2e05dd57
Fernando Maytorena E <113787130+a01110946@users.noreply.github.com>
2023-05-28 16:26:12 -0600

    Add files via upload

--- tec_de_monterrey/main.py
+++ tec_de_monterrey/main.py
@@ -8,7 +8,7 @@ from langchain.llms import OpenAI
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
+    llm = OpenAI(openai_api_key=st.secrets["OPENAI_API_KEY"] temperature=0)
     chain = ConversationChain(llm=llm)
     return chain
 
