3414ea7e960c3c9f37341d1ce63505177141323f
Fernando Maytorena E <113787130+a01110946@users.noreply.github.com>
2023-06-11 16:19:35 -0600

    Add files via upload

--- tec_de_monterrey/main.py
+++ tec_de_monterrey/main.py
@@ -4,23 +4,8 @@ from streamlit_chat import message
 
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
-from langchain.schema import BaseOutputParser
-from typing import Any
-from langchain.llms import Cohere
-from langchain import PromptTemplate
-import re
-from langchain.agents.mrkl.prompt import FORMAT_INSTRUCTIONS
-from langchain.agents import ConversationalChatAgent
-from langchain.agents import AgentExecutor
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage,
-    BaseMessage,
-)
 
 from langchain.agents import create_pandas_dataframe_agent
-from langchain.agents import create_csv_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import Tool, AgentType, initialize_agent
 from langchain.memory import ConversationBufferMemory
@@ -28,9 +13,6 @@ from langchain.utilities import PythonREPL
 import pandas as pd
 import requests
 import urllib.request
-from langchain.chains import SimpleSequentialChain
-from langchain.prompts import ChatPromptTemplate
-from langchain.chains import LLMChain
 
 # OPENAI_API_KEY ENVIROMENTAL VARIABLE 
 import os
@@ -40,15 +22,9 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 #AGREGADO PARA IMAGENES
 from PIL import Image
-#aqui va la ruta real que pondremos en github
-#sustituir esta linea cuando la imagen la subas
-#image = Image.open('/users/sofia/downloads/tecnologico-de-monterrey-blue.png')
 urllib.request.urlretrieve('https://raw.githubusercontent.com/a01110946/chatbot/main/tec_de_monterrey/tecnologico_de_monterrey-blue.jpeg', 'logo_tec_de_monterrey')
 image = Image.open('logo_tec_de_monterrey')
 ### FIN DE AGREGADO PARA IMAGENES
-
-
-
 
 # GitHub file URL
 file_url = "https://raw.githubusercontent.com/a01110946/chatbot/main/tec_de_monterrey/Carreras_profesionales.csv"
@@ -61,35 +37,36 @@ with open("Carreras_profesionales.csv", "wb") as file:
     file.write(response.content)
 
 # Read the downloaded file using Pandas
-#df = pd.read_excel("Corpus_de_informacion.xlsx", sheet_name='Oferta académica', header=0, dtype={'Nombre del Programa': str, 'Tipo de Programa': str, 'Escuela': str, 'Campus': list, 'Duración': str, 'Periodo': str}, engine='openpyxl')
-
 df = pd.read_csv(filepath_or_buffer='Carreras_profesionales.csv', sep=",", header=0, encoding='latin-1')
 
-
-# Split the values in the column based on comma and pipe delimiters
-df['Campus'] = df['Campus'].astype(str).str.split('; ')
-#df['Plan de Estudios'] = df['Plan de Estudios'].astype(str).str.split('|')
+# Split the values in the column based on comma delimiter
+df['Campus'] = df['Campus'].str.split(', ')
 
 # Convert the split values into a list of strings
 df['Campus'] = df['Campus'].apply(lambda x: [str(value).strip() for value in x])
-#df['Plan de Estudios'] = df['Plan de Estudios'].apply(lambda x: [str(value).strip() for value in x])
 
-#def tec_de_monterrey_agent_tool(input):
-#    pandas_agent = create_pandas_dataframe_agent(ChatOpenAI(temperature=0), df, verbose=True)
-#    return pandas_agent.run(input)
+def tec_de_monterrey_agent_tool(input):
+    pandas_agent = create_pandas_dataframe_agent(ChatOpenAI(temperature=0), df, verbose=True)
+    return pandas_agent.run(input)
 
-def get_answer_csv(query: str, df=df) -> str:
+python_repl = PythonREPL()
 
-    chat = ChatOpenAI(temperature=0, verbose=True)
+tools = [
+Tool(
+    name="Tecnológico de Monterrey Agent",
+    func=tec_de_monterrey_agent_tool,
+    description="A tool to retrieve information from Tecnológico de Monterrey. Always assume you need to use this tool to get information from the Tec. Always answer in Spanish.",
+),
+Tool(
+    name="python_repl",
+    description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
+    func=python_repl.run
+)
+]
 
-    csv_agent = create_pandas_dataframe_agent(llm=chat,
-                                            df=df,
-                                            verbose=True)
-    
-    response = csv_agent.run(query)
+memory = ConversationBufferMemory(memory_key="chat_history")
 
-    return response
-
+agent_chain = initialize_agent(tools=tools, llm=ChatOpenAI(temperature=0), agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
 
 
 # SECCION DE ENCABEZADOS Y PANTALLA DE INICIO
@@ -111,7 +88,6 @@ Realiza la preguntas a nuestro Chatbot.
 ###### FIN DE PANTALLA DE INICIO
 
 
-
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
 
@@ -120,16 +96,14 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("Tú: ",
-                               "Hola, enlista la oferta completa de carreras profesionales del Tec.", key="input")
+    input_text = st.text_input("Tú: ", "Hola, ¿cómo estás?", key="input")
     return input_text
 
 
 user_input = get_text()
 
 if user_input:
-    output = get_answer_csv(query=user_input)
-
+    output = agent_chain.run(input=user_input)
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
@@ -139,5 +113,3 @@ if st.session_state["generated"]:
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
-        
-        
