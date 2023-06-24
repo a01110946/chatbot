d9de9de428e6b28aed9f078fdee0114422b2992c
Fernando Maytorena E <113787130+a01110946@users.noreply.github.com>
2023-06-24 17:03:12 -0600

    Add files via upload

--- tec_de_monterrey/main.py
+++ tec_de_monterrey/main.py
@@ -97,8 +97,31 @@ with st.container():
 ##### PRUEBA #####
 class NewAgentOutputParser(BaseOutputParser):
     def get_format_instructions(self) -> str:
-        parser = PydanticOutputParser()
-        FORMAT_INSTRUCTIONS = parser.get_format_instructions()
+        FORMAT_INSTRUCTIONS = """RESPONSE FORMAT INSTRUCTIONS
+        ----------------------------
+
+        When responding to me please, please output a response in one of two formats:
+
+        **Option 1:**
+        Use this if you want the human to use a tool.
+        Markdown code snippet formatted in the following schema:
+
+        ```json
+        {{{{
+            "action": string \\ The action to take. Must be one of {tool_names}
+            "action_input": string \\ The input to the action
+        }}}}
+        ```
+
+        **Option #2:**
+        Use this if you want to respond directly to the human. Markdown code snippet formatted in the following schema:
+
+        ```json
+        {{{{
+            "action": "Final Answer",
+            "action_input": string \\ You should put what you want to return to use here
+        }}}}
+        ```"""
         return FORMAT_INSTRUCTIONS
 
     def parse(self, text: str) -> Any:
@@ -144,12 +167,20 @@ class NewAgentOutputParser(BaseOutputParser):
         return {"action": response["action"], "action_input": response["action_input"]}
         # end of problematic code
 
+SYSTEM_MESSAGE = """Assistant is a large language model trained by OpenAI.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful system that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist."""
+
 def make_chain():
     memory = ConversationBufferMemory(
         memory_key="chat_history", return_messages=True)
 
     agent = ConversationalChatAgent.from_llm_and_tools(
-        llm=ChatOpenAI(), tools=[], memory=memory, verbose=True, output_parser=NewAgentOutputParser())
+        llm=ChatOpenAI(), tools=[], system_message=SYSTEM_MESSAGE, memory=memory, verbose=True, output_parser=NewAgentOutputParser())
 
     agent_chain = AgentExecutor.from_agent_and_tools(
         agent=agent,
