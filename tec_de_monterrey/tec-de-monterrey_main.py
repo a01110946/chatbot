b5cdee617b701b2d56b7042edabeb79aa5fb3695
Fernando Maytorena E <113787130+a01110946@users.noreply.github.com>
2023-09-03 12:30:30 -0700

    Update tec-de-monterrey_main.py

--- tec_de_monterrey/tec-de-monterrey_main.py
+++ tec_de_monterrey/tec-de-monterrey_main.py
@@ -8,6 +8,27 @@ import requests
 import urllib.request
 from PIL import Image
 
+# Streamlit UI.
+st.set_page_config(page_title="Tec de Monterrey - Chatbot", page_icon=":robot:", layout="wide")
+with st.container():  
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.image(image,use_column_width='auto')#despliega logo
+        st.header('Tec ChatBot')
+        st.markdown("Many \"question-answering\" tools focus on unstructured text, yet much data is tabular. We've created a demo app to query table-based data using a dataset from Tecnológico de Monterrey's website. Take a look at the dataset [here](https://github.com/a01110946/chatbot/blob/main/tec_de_monterrey/Tecnologico-de-Monterrey_Curriculum.csv) so you know what type of questions you can ask!\n\nThis is not an official app and is not endorsed by Tecnológico de Monterrey.")
+    with right_column:
+        st.image(image2,use_column_width='auto')
+
+st.sidebar.header('Hi, welcome!')
+st.sidebar.markdown("""
+The app's goal is to answer your questions regarding professional careers
+and postgraduate courses offered by Tecnológico de Monterrey.
+    
+Ask questions to our Chatbot.
+""")
+st.title('Tec Chatbot')
+st.info("TecChat Bot can provide answers to most of your questions regarding Tecnológico de Monterrey's curriculum.")
+
 """
 # Initialize session state for conversation history
 if "generated" not in st.session_state:
@@ -32,27 +53,6 @@ image = Image.open('logo_tec_de_monterrey')
 
 urllib.request.urlretrieve('https://raw.githubusercontent.com/a01110946/chatbot/main/tec_de_monterrey/agent-v1.png', 'agent-image')
 image2 = Image.open('agent-image')
-
-# Streamlit UI.
-st.set_page_config(page_title="Tec de Monterrey - Chatbot", page_icon=":robot:", layout="wide")
-with st.container():  
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.image(image,use_column_width='auto')#despliega logo
-        st.header('Tec ChatBot')
-        st.markdown("Many \"question-answering\" tools focus on unstructured text, yet much data is tabular. We've created a demo app to query table-based data using a dataset from Tecnológico de Monterrey's website. Take a look at the dataset [here](https://github.com/a01110946/chatbot/blob/main/tec_de_monterrey/Tecnologico-de-Monterrey_Curriculum.csv) so you know what type of questions you can ask!\n\nThis is not an official app and is not endorsed by Tecnológico de Monterrey.")
-    with right_column:
-        st.image(image2,use_column_width='auto')
-
-st.sidebar.header('Hi, welcome!')
-st.sidebar.markdown("""
-The app's goal is to answer your questions regarding professional careers
-and postgraduate courses offered by Tecnológico de Monterrey.
-    
-Ask questions to our Chatbot.
-""")
-st.title('Tec Chatbot')
-st.info("TecChat Bot can provide answers to most of your questions regarding Tecnológico de Monterrey's curriculum.")
 
 """
 query_text = st.text_input('Enter your question:', placeholder = 'In which campus is architecture offered?')
